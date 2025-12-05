ae90a533a86785efbf8015b1ec6fe31802a625da
pio-o-connell <pio_o_con@hotmail.com>
2025-12-05 10:23:25 +0000

    Configure sqlite local and postgres remote

--- config/settings.py
+++ config/settings.py
@@ -233,7 +233,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
-# Default primary key field type
+# Whitenoise static file serving    
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# Default primary key field type    
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

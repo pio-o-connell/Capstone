4703df3caf87d175fe2c1bdc245b646f74bbc060
pio-o-connell <pio_o_con@hotmail.com>
2025-12-08 12:02:15 +0000

    Add logged in status banner

--- core/static/core/js/header-offset.js
+++ core/static/core/js/header-offset.js
@@ -1,11 +1,23 @@
 // Measure the fixed header height and set --header-height CSS variable on <html>
+// Also position the login banner below the header
 (function () {
   function updateHeaderOffset() {
     try {
       var header = document.querySelector('header');
+      var banner = document.querySelector('.login-banner-wrapper');
       if (!header) return;
-      var height = header.offsetHeight;
-      document.documentElement.style.setProperty('--header-height', height + 'px');
+      
+      var headerHeight = header.offsetHeight;
+      
+      // Position banner right below header
+      if (banner) {
+        banner.style.top = headerHeight + 'px';
+        var bannerHeight = banner.offsetHeight;
+        var totalHeight = headerHeight + bannerHeight;
+        document.documentElement.style.setProperty('--header-height', totalHeight + 'px');
+      } else {
+        document.documentElement.style.setProperty('--header-height', headerHeight + 'px');
+      }
     } catch (e) {
       // ignore
     }
@@ -23,20 +35,28 @@
       updateHeaderOffset();
       window.addEventListener('resize', onResize);
 
-      // Observe header mutations (class changes or content) that may affect height
+      // Observe header and banner mutations (class changes or content) that may affect height
       var header = document.querySelector('header');
+      var banner = document.querySelector('.login-banner-wrapper');
       if (header && window.MutationObserver) {
         var mo = new MutationObserver(function () { updateHeaderOffset(); });
         mo.observe(header, { attributes: true, childList: true, subtree: true });
+        if (banner) {
+          mo.observe(banner, { attributes: true, childList: true, subtree: true });
+        }
       }
     });
   } else {
     updateHeaderOffset();
     window.addEventListener('resize', onResize);
     var header = document.querySelector('header');
+    var banner = document.querySelector('.login-banner-wrapper');
     if (header && window.MutationObserver) {
       var mo = new MutationObserver(function () { updateHeaderOffset(); });
       mo.observe(header, { attributes: true, childList: true, subtree: true });
+      if (banner) {
+        mo.observe(banner, { attributes: true, childList: true, subtree: true });
+      }
     }
   }
 })();

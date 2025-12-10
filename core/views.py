463ffd2b8a33c6a3a617389f186ef3085855f068
pio-o-connell <pio_o_con@hotmail.com>
2025-12-10 01:52:55 +0000

    Final Fix for PEP8 standard all apps using flake8

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
+
 def home(request):
     """
     Homepage view: renders the homepage template.
     """
     return render(request, 'core/home.html')
+
 
 def contact(request):
     """
